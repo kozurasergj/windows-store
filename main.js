8f472dabd975dc693964e5057a0c41f7b7d1f604
kozurasergj <kozura.sergj33@gmail.com>
2023-03-04 11:32:33 +0200

    form submit

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 import './modules/slider';
-import { modals, tabs } from "./modules";
+import { modals, tabs, form } from "./modules";
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
-
   tabs({
     headerSelector: '.glazing_slider',
     tabsSelector: '.glazing_block',
     contentSelector: '.glazing_content',
     activeClass: 'active',
   });
-
   tabs({
     headerSelector: '.decoration_slider',
     tabsSelector: '.no_click',
     contentSelector: '.decoration_content > div > div',
     activeClass: 'after_click'
   });
+  form();
 });
