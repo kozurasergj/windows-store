15d76caaddb85e0364098775a179a2d565dbf0e1
kozurasergj <kozura.sergj33@gmail.com>
2023-03-07 16:03:43 +0200

    new up-date

--- modules/timer.js
+++ modules/timer.js
@@ -10,29 +10,29 @@ export const timer = (selector, deadline) => {
     return { betweenTime, seconds, minutes, hours, days };
   }
 
-  const updateClock = () => {
-    const timeObj = getTimeRemaining(deadline);
-    days.textContent = addZero(timeObj.days);
-    hours.textContent = addZero(timeObj.hours);
-    minutes.textContent = addZero(timeObj.minutes);
-    seconds.textContent = addZero(timeObj.seconds);
-    if (timeObj.totalMilliseconds <= 0) {
-      days.textContent = "00";
-      hours.textContent = "00";
-      minutes.textContent = "00";
-      seconds.textContent = "00";
-      clearInterval(timerInterval);
-    }
-  };
-  updateClock();
-  const setClock = (selector, deadline) => {
-    const HtmlblockTimer = document.querySelector(selector);
-    const days = HtmlblockTimer.querySelector("#days");
-    const hours = HtmlblockTimer.querySelector("#hours");
-    const minutes = HtmlblockTimer.querySelector("#minutes");
-    const seconds = HtmlblockTimer.querySelector("#seconds");
-    const timerInterval = setInterval(updateClock, 1000);
-  };
+    const updateClock = () => {
+      const timeObj = getTimeRemaining(deadline);
+      days.textContent = addZero(timeObj.days);
+      hours.textContent = addZero(timeObj.hours);
+      minutes.textContent = addZero(timeObj.minutes);
+      seconds.textContent = addZero(timeObj.seconds);
+      if (timeObj.totalMilliseconds <= 0) {
+        days.textContent = "00";
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+        clearInterval(timerInterval);
+      }
+    };
+    updateClock();
+    const setClock = (selector, deadline) => {
+      const HtmlblockTimer = document.querySelector(selector);
+      const days = HtmlblockTimer.querySelector("#days");
+      const hours = HtmlblockTimer.querySelector("#hours");
+      const minutes = HtmlblockTimer.querySelector("#minutes");
+      const seconds = HtmlblockTimer.querySelector("#seconds");
+      const timerInterval = setInterval(updateClock, 1000);
+    };
 
-  setClock(selector, deadline);
-}
+    setClock(selector, deadline);
+  }
